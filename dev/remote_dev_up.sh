f92f0a711e89e9bf119e5987f720e0a5d0c46608
cyprian <csniegota@yameo.pl>
2020-10-24 22:55:08 +0200

    remote deploy script, web

---
+++ dev/remote_dev_up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Note: require DEV_DOCKER_USER to be set for remote and ssh-copy-key on dev server
+# param - dev server host name
+
+# DEV_DOCKER_USER=???
+DEV_SERVER_ADDRESS=$1
+echo ${DEV_SERVER_ADDRESS}
+
+DOCKER_HOST="ssh://${DEV_DOCKER_USER}@${DEV_SERVER_ADDRESS}" docker-compose up -d
