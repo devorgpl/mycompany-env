d24a056290e9d479c5c4db7731694953b9716898
cyprian <csniegota@yameo.pl>
2020-10-25 15:43:53 +0100

    stick to version

---
+++ dev/remote_dev_full.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Note: require DEV_DOCKER_USER to be set for remote and ssh-copy-key on dev server
+# param - dev server host name
+
+# DEV_DOCKER_USER=???
+DEV_SERVER_ADDRESS=$1
+echo ${DEV_SERVER_ADDRESS}
+
+DOCKER_HOST="ssh://${DEV_DOCKER_USER}@${DEV_SERVER_ADDRESS}" docker-compose pull
+DOCKER_HOST="ssh://${DEV_DOCKER_USER}@${DEV_SERVER_ADDRESS}" docker-compose build
+DOCKER_HOST="ssh://${DEV_DOCKER_USER}@${DEV_SERVER_ADDRESS}" docker-compose up -d --remove-orphans
+DOCKER_HOST="ssh://${DEV_DOCKER_USER}@${DEV_SERVER_ADDRESS}" docker-compose restart revproxy
+
